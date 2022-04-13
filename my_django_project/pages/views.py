e364b8b66ff0f531d9ab8c89fd8a9aed996f3b73
omertb <omertugrul@gmail.com>
2022-04-14 00:17:24 +0300

    templates dir

--- my_django_project/pages/views.py
+++ my_django_project/pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>index</h1>')
+    return render(request, 'index.html')
+    #return HttpResponse('<h1>index</h1>')
 
 # Create your views here.
